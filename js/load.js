d2d9dc1a41409b7a1c5b32409a239707c24e439e
Christian Loga <148154322+PlayFlixo@users.noreply.github.com>
2024-07-29 13:48:26 -0500

    Update load.js

--- js/load.js
+++ js/load.js
@@ -3,9 +3,6 @@ if (params.get("game")) {
     games.forEach(async game => {
         if (game.id != params.get("game")) return
         document.title = `${game.title} | Art Class`
-        document.querySelector("#gameImage").src = game.image
-        document.querySelector("#gameTitle").innerHTML = game.title
-        if (game.description) document.querySelector("#gameDescription").innerHTML = game.description
         document.querySelector("#frame").src = __uv$config.prefix + __uv$config.encodeUrl(game.url)
 
 
