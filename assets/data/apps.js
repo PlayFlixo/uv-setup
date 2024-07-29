20f7549df6b206b4262543f4c8e42fe62cddbe57
Christian Loga <148154322+PlayFlixo@users.noreply.github.com>
2024-07-29 13:10:03 -0500

    Update apps.js

--- assets/data/apps.js
+++ assets/data/apps.js
@@ -1,14 +1,7 @@
 var apps = [
     {
-      'id': 'customapp',
-      'title': 'Add custom app',
-      'image': 'assets/images/add.png'
-    },
-    {
       "id": "google",
-      "title": "Google",
       "url": "https://www.google.com/?safe=active&ssui=on",
-      'image': 'https://img.freepik.com/free-icon/search_318-265146.jpg'
       
     },
     {
