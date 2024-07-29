4dd0a51baa5487be4ac47fa929c6f49880b228cc
Christian Loga <148154322+PlayFlixo@users.noreply.github.com>
2024-07-29 13:58:01 -0500

    Update load.js

--- js/load.js
+++ js/load.js
@@ -1,68 +1,101 @@
-const params = new URLSearchParams(window.location.search)
+const params = new URLSearchParams(window.location.search);
+
 if (params.get("game")) {
     games.forEach(async game => {
-        if (game.id != params.get("game")) return
-        document.title = `${game.title} | Art Class`
-        document.querySelector("#frame").src = __uv$config.prefix + __uv$config.encodeUrl(game.url)
+        if (game.id != params.get("game")) return;
+        document.title = `${game.title} | Art Class`;
+        
+        if (game.image) {
+            const gameImage = document.querySelector("#gameImage");
+            gameImage.src = game.image;
+            gameImage.classList.remove("hidden");
+        }
+        
+        if (game.title) {
+            const gameTitle = document.querySelector("#gameTitle");
+            gameTitle.innerHTML = game.title;
+            gameTitle.classList.remove("hidden");
+        }
+        
+        if (game.description) {
+            const gameDescription = document.querySelector("#gameDescription");
+            gameDescription.innerHTML = game.description;
+            gameDescription.classList.remove("hidden");
+        }
 
-
-    })
+        document.querySelector("#frame").src = __uv$config.prefix + __uv$config.encodeUrl(game.url);
+    });
 } else if (params.get("app")) {
-
     apps.forEach(app => {
-        if (app.id != params.get("app")) return
-        document.title = `${app.title} | Art Class`
-        document.querySelector("#gameImage").src = app.image
-        document.querySelector("#gameTitle").innerHTML = app.title
-        if (app.description) document.querySelector("#gameDescription").innerHTML = app.description
+        if (app.id != params.get("app")) return;
+        document.title = `${app.title} | Art Class`;
+        
+        if (app.image) {
+            const gameImage = document.querySelector("#gameImage");
+            gameImage.src = app.image;
+            gameImage.classList.remove("hidden");
+        }
+        
+        if (app.title) {
+            const gameTitle = document.querySelector("#gameTitle");
+            gameTitle.innerHTML = app.title;
+            gameTitle.classList.remove("hidden");
+        }
+        
+        if (app.description) {
+            const gameDescription = document.querySelector("#gameDescription");
+            gameDescription.innerHTML = app.description;
+            gameDescription.classList.remove("hidden");
+        }
 
         document.querySelector("#frame").src = __uv$config.prefix + __uv$config.encodeUrl(app.url);
-    })
+    });
 }
 
-if (!getObj("favoritedGames")) setObj("favoritedGames", [])
-if (!getObj("favoritedApps")) setObj("favoritedApps", [])
+if (!getObj("favoritedGames")) setObj("favoritedGames", []);
+if (!getObj("favoritedApps")) setObj("favoritedApps", []);
 
-var favoritedButton = document.querySelector(".favorited")
-var favoritedGames = getObj("favoritedGames")
-var favoritedApps = getObj("favoritedApps")
+const favoritedButton = document.querySelector(".favorited");
+const favoritedGames = getObj("favoritedGames");
+const favoritedApps = getObj("favoritedApps");
 
-var game = params.get("game")
-var app = params.get("app")
+const game = params.get("game");
+const app = params.get("app");
 
 if (favoritedGames.includes(game)) {
-    favoritedButton.classList.remove("far")
-    favoritedButton.classList.add("fas")
+    favoritedButton.classList.remove("far");
+    favoritedButton.classList.add("fas");
 }
 
-if (favoritedGames.includes(game)) {
-    favoritedButton.classList.remove("far")
-    favoritedButton.classList.add("fas")
+if (favoritedApps.includes(app)) {
+    favoritedButton.classList.remove("far");
+    favoritedButton.classList.add("fas");
 }
+
 function favorite() {
     if (game) {
-        var index = favoritedGames.indexOf(game);
+        const index = favoritedGames.indexOf(game);
         if (index !== -1) {
             favoritedGames.splice(index, 1);
-            favoritedButton.classList.remove("fas")
-            favoritedButton.classList.add("far")
+            favoritedButton.classList.remove("fas");
+            favoritedButton.classList.add("far");
         } else {
-            favoritedGames.push(game)
-            favoritedButton.classList.remove("far")
-            favoritedButton.classList.add("fas")
+            favoritedGames.push(game);
+            favoritedButton.classList.remove("far");
+            favoritedButton.classList.add("fas");
         }
         setObj("favoritedGames", favoritedGames);
     } else if (app) {
-        var index = favoritedGames.indexOf(game);
+        const index = favoritedApps.indexOf(app);
         if (index !== -1) {
-            favoritedGames.splice(index, 1);
-            favoritedButton.classList.remove("fas")
-            favoritedButton.classList.add("far")
+            favoritedApps.splice(index, 1);
+            favoritedButton.classList.remove("fas");
+            favoritedButton.classList.add("far");
         } else {
-            favoritedGames.push(game)
-            favoritedButton.classList.remove("far")
-            favoritedButton.classList.add("fas")
+            favoritedApps.push(app);
+            favoritedButton.classList.remove("far");
+            favoritedButton.classList.add("fas");
         }
-        setObj("favoritedGames", favoritedGames);
+        setObj("favoritedApps", favoritedApps);
     }
 }
